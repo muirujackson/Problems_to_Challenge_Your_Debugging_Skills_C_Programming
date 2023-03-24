43d939ce82bfc4e95ffebf8af5a7cd8a79aaf734
jakezweru <jakezweru@gmail.com>
2023-03-24 03:17:05 -0400

    added description of willis product

--- Exercise Files/Ch01/01_05/01_05-challenge1.c
+++ Exercise Files/Ch01/01_05/01_05-challenge1.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * This is code is solving wallis Product
+ * check out this link https://mindyourdecisions.com/blog/2016/10/12/the-wallis-product-formula-for-pi-and-its-proof/#:~:text=Each%20new%20pair%20is%20%5B2n,come%20from%20work%20in%201655.
+ */
+
 float wallis(float a)
 {
 	return(a/a-1.0) * (a/a+1.0);
